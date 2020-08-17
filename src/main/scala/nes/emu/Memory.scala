55292398ce13a1272d003e7a79a657a52f4eed68
Ezra Serebrin <ezraserebrin@gmail.com>
2020-08-16 23:22:09 -0500

    Work on ROM support

--- src/main/scala/nes/emu/Memory.scala
+++ src/main/scala/nes/emu/Memory.scala
@@ -9,39 +9,64 @@ class Memory(cartridgeFileName: String) {
 
     private val CartridgeSpaceSize = 0xBFE0
 
-    // Read ROM contents from file
+    /* --- Read ROM contents from file --- */
+
     private var romContents = mutable.Buffer[Int]()
     private val CartridgeFileInstream = new BufferedInputStream(new FileInputStream(new File(cartridgeFileName)))
-    while(CartridgeFileInstream.available > 0) romContents += CartridgeFileInstream.read()
+    while (CartridgeFileInstream.available > 0) romContents += CartridgeFileInstream.read()
     CartridgeFileInstream.close()
 
-    private var ram = Array.fill(0x800)(0)
-    private var cartridge = romContents ++ Array.fill(CartridgeSpaceSize - romContents.size)(0)
-    private var self = ram ++ Array.fill(0x10000 - 0xBFE0 - 0x800)(0) ++ cartridge
 
+    /* --- Examine Header --- */
 
-    /* --- Header initialization --- */
-
-    private val header = cartridge.take(16)
+    private val Header = romContents.take(16).toVector
+    romContents --= Header
 
     // Determine file format is correct
     private var iNESFormat = false
     private var iNES2Format = false
 
-    if (header(0) == 'N' && header(1) == 'E' && header(2) == 'S' && header(3) == 0x1A) iNESFormat = true
-    if (iNESFormat && (header(7) & 0x0C) == 0x08) iNES2Format = true
+    if (Header(0) == 'N' && Header(1) == 'E' && Header(2) == 'S' && Header(3) == 0x1A) iNESFormat = true
+    if (iNESFormat && (Header(7) & 0x0C) == 0x08) iNES2Format = true
 
     if (!iNESFormat && !iNES2Format) {
         println("Invalid ROM type.\niNES and NES 2.0 File Formats are supported.")
         sys.exit(1)
     }
 
-    private val trainerArea = if ((header(6) & 2) >> 1 == 1) cartridge.slice(16, 16 + 512) else Array[Int]()
+    /* --- Create various ROM components --- */
 
-    // PRG/CHR ROM size
-    val PRGROMSize = header(4) 
-    val CHRROMSizeLowByte = header(5)
+    private val Trainer = if ((Header(6) & 2) >> 1 == 1) romContents.take(512).toVector
+                          else Vector[Int]()
+    romContents --= Trainer
 
+    /* --- PRGROM --- */
+
+    // Detemine size of PRGROM
+    private var PRGROMSize = 0
+    if (Header(9) == 0xF) {
+        // Use exponent representation for PRGROM size
+        val Exponent = Math.pow(Header(4) & 0xFC, 2)
+        val Multiplier = (Header(4) & 3) * 2 + 1
+        PRGROMSize = Exponent.toInt * Multiplier
+    } else {
+        val HighNibble = Header(9) & 0xF
+        (HighNibble << 8) | Header(4) 
+    }
+
+    private val PRGROM = romContents.take(PRGROMSize).toVector
+    romContents --= PRGROM
+
+
+    private val CHRROMSizeLowByte = Header(5)
+    /* --- Assemble the memory! --- */
+
+    private var ram = Array.fill(0x800)(0)
+
+    // TODO: Mappers
+    private var cartridge = Trainer ++ PRGROM
+
+    private var self = ram ++ Array.fill(0x10000 - 0xBFE0 - 0x800)(0) ++ cartridge
 
     def apply(pointer: Int): Int = self(pointer)
     def update(pointer: Int, replacement: Int) = self(pointer) = replacement
