9a334d6d97fe8d664fc6760bc184bfa60b206181
Ezra Serebrin <ezraserebrin@gmail.com>
2020-08-17 12:31:30 -0500

    Work on ROM Support

--- src/main/scala/nes/emu/Memory.scala
+++ src/main/scala/nes/emu/Memory.scala
@@ -7,68 +7,65 @@ import java.io.File
 
 class Memory(cartridgeFileName: String) {
 
-    private val CartridgeSpaceSize = 0xBFE0
-
-    /* --- Read ROM contents from file --- */
-
-    private var romContents = mutable.Buffer[Int]()
-    private val CartridgeFileInstream = new BufferedInputStream(new FileInputStream(new File(cartridgeFileName)))
-    while (CartridgeFileInstream.available > 0) romContents += CartridgeFileInstream.read()
-    CartridgeFileInstream.close()
-
-
-    /* --- Examine Header --- */
-
-    private val Header = romContents.take(16).toVector
-    romContents --= Header
-
-    // Determine file format is correct
-    private var iNESFormat = false
-    private var iNES2Format = false
-
-    if (Header(0) == 'N' && Header(1) == 'E' && Header(2) == 'S' && Header(3) == 0x1A) iNESFormat = true
-    if (iNESFormat && (Header(7) & 0x0C) == 0x08) iNES2Format = true
-
-    if (!iNESFormat && !iNES2Format) {
-        println("Invalid ROM type.\niNES and NES 2.0 File Formats are supported.")
-        sys.exit(1)
-    }
-
-    /* --- Create various ROM components --- */
-
-    private val Trainer = if ((Header(6) & 2) >> 1 == 1) romContents.take(512).toVector
-                          else Vector[Int]()
-    romContents --= Trainer
-
-    /* --- PRGROM --- */
-
-    // Detemine size of PRGROM
-    private var PRGROMSize = 0
-    if (Header(9) == 0xF) {
-        // Use exponent representation for PRGROM size
-        val Exponent = Math.pow(Header(4) & 0xFC, 2)
-        val Multiplier = (Header(4) & 3) * 2 + 1
-        PRGROMSize = Exponent.toInt * Multiplier
-    } else {
-        val HighNibble = Header(9) & 0xF
-        (HighNibble << 8) | Header(4) 
-    }
-
-    private val PRGROM = romContents.take(PRGROMSize).toVector
-    romContents --= PRGROM
-
-
-    private val CHRROMSizeLowByte = Header(5)
-    /* --- Assemble the memory! --- */
-
+    private val cartridge = createCartridge(getROMFileContents())
     private var ram = Array.fill(0x800)(0)
-
-    // TODO: Mappers
-    private var cartridge = Trainer ++ PRGROM
-
     private var self = ram ++ Array.fill(0x10000 - 0xBFE0 - 0x800)(0) ++ cartridge
+
 
     def apply(pointer: Int): Int = self(pointer)
     def update(pointer: Int, replacement: Int) = self(pointer) = replacement
+
+
+    private def getROMFileContents(): Buffer[Int] = {
+        var romContents = mutable.Buffer[Int]()
+        val CartridgeFileInstream = new BufferedInputStream(new FileInputStream(new File(cartridgeFileName)))
+        while (CartridgeFileInstream.available > 0) romContents += CartridgeFileInstream.read()
+        CartridgeFileInstream.close()
+        romContents
+    }
+
+    // TODO: Mappers
+    private def createCartridge(romContents: Buffer[Int]): Array[Int] = {
+        val Header = romContents.take(16).toVector
+        romContents --= Header
+
+        ensureCorrectFileFormat(Header)
+        
+        val Trainer = if ((Header(6) & 2) >> 1 == 1) romContents.take(512).toVector
+                            else Vector[Int]()
+        romContents --= Trainer
+
+        val PRGROMSize = getROMAreaSize(lowByte = Header(4), highNibble = Header(9) & 0xF)
+        val PRGROM = romContents.take(PRGROMSize).toVector
+        romContents --= PRGROM
+
+        val CHRROMSize = getROMAreaSize(lowByte = Header(5), highNibble = Header(9) & 0xF0)
+        val CHRROM = romContents.take(CHRROMSize).toVector
+        romContents --= CHRROM
+
+        Trainer ++ PRGROM ++ CHRROM ++ romContents
+    }
+
+    private def ensureCorrectFileFormat(header: Vector[Int]): Unit = {
+        var iNESFormat = false
+        var iNES2Format = false
+
+        if (header(0) == 'N' && header(1) == 'E' && header(2) == 'S' && header(3) == 0x1A) iNESFormat = true
+        if (iNESFormat && (header(7) & 0x0C) == 0x08) iNES2Format = true
+
+        if (!iNESFormat && !iNES2Format) {
+            println("Invalid ROM type.\niNES and NES 2.0 File Formats are supported.")
+            sys.exit(1)
+        }
+    }
+
+    private def getROMAreaSize(lowByte: Nibble, highNibble: Int): Int = {
+        if (highNibble == 0xF) {
+            val Exponent = Math.pow(lowByte & 0xFC, 2)
+            val Multiplier = (lowByte & 3) * 2 + 1
+            Exponent.toInt * Multiplier
+        }
+        else (highNibble << 8) | lowByte
+    }
 
 }
