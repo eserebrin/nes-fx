ee0acfe9d82b73aed12983f6c9081622d8d081a1
Ezra Serebrin <ezraserebrin@gmail.com>
2020-08-21 15:39:03 -0500

    Work on Cartridge

--- src/main/scala/nes/emu/Memory.scala
+++ src/main/scala/nes/emu/Memory.scala
@@ -7,16 +7,23 @@ import java.io.File
 
 class Memory(cartridgeFileName: String) {
 
-    private val cartridge = createCartridge(getROMFileContents())
-    private var ram = Array.fill(0x800)(0)
-    private var self = ram ++ Array.fill(0x10000 - 0xBFE0 - 0x800)(0) ++ cartridge
+    private val MapperNumber = (Header(6) & 0xF0) >> 4
+
+    private val Cartridge = createCartridge(getROMFileContents())
+    private val RAM = Array.fill(0x800)(0)
+    private val PPURegisters = Array.fill(8)(0)
+    private val PPURegisterMirrrors = PPURegisters * 8
+    private val APURegisters = Array.fill(0x18)(0)
+    private val APUArea = Array.fill(8)(0)
+    private var MemoryComponents = Vector(RAM, RAM, RAM, PPURegisters, PPURegisterMirrrors, APURegisters, APUArea, Cartridge)
+    private var self = createAddressableSpace(MapperNumber, MemoryComponents)
 
 
     def apply(pointer: Int): Int = self(pointer)
-    def update(pointer: Int, replacement: Int) = self(pointer) = replacement
+    def update(pointer: Int, replacement: Int): Unit = self(pointer) = replacement
 
 
-    private def getROMFileContents(): Buffer[Int] = {
+    private def getROMFileContents(): mutable.Buffer[Int] = {
         var romContents = mutable.Buffer[Int]()
         val CartridgeFileInstream = new BufferedInputStream(new FileInputStream(new File(cartridgeFileName)))
         while (CartridgeFileInstream.available > 0) romContents += CartridgeFileInstream.read()
@@ -25,21 +32,21 @@ class Memory(cartridgeFileName: String) {
     }
 
     // TODO: Mappers
-    private def createCartridge(romContents: Buffer[Int]): Array[Int] = {
+    private def createCartridge(romContents: mutable.Buffer[Int]): Array[Int] = {
         val Header = romContents.take(16).toVector
         romContents --= Header
 
         ensureCorrectFileFormat(Header)
         
         val Trainer = if ((Header(6) & 2) >> 1 == 1) romContents.take(512).toVector
-                            else Vector[Int]()
+                      else Vector[Int]()
         romContents --= Trainer
 
         val PRGROMSize = getROMAreaSize(lowByte = Header(4), highNibble = Header(9) & 0xF)
         val PRGROM = romContents.take(PRGROMSize).toVector
         romContents --= PRGROM
 
-        val CHRROMSize = getROMAreaSize(lowByte = Header(5), highNibble = Header(9) & 0xF0)
+        val CHRROMSize = getROMAreaSize(lowByte = Header(5), highNibble = (Header(9) & 0xF0) >> 4)
         val CHRROM = romContents.take(CHRROMSize).toVector
         romContents --= CHRROM
 
@@ -59,9 +66,9 @@ class Memory(cartridgeFileName: String) {
         }
     }
 
-    private def getROMAreaSize(lowByte: Nibble, highNibble: Int): Int = {
+    private def getROMAreaSize(lowByte: Int, highNibble: Int): Int = {
         if (highNibble == 0xF) {
-            val Exponent = Math.pow(lowByte & 0xFC, 2)
+            val Exponent = Math.pow((lowByte & 0xFC) >> 2, 2)
             val Multiplier = (lowByte & 3) * 2 + 1
             Exponent.toInt * Multiplier
         }
